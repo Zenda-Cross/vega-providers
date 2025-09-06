aa7c517f4d3aff34394215acc542d4c408c534b8
himanshu8443 <sangwanhimanshu8443@gmail.com>
2025-09-06 16:28:01 +0530

    fix: update getStream function to remove hardcoded cookie and enhance photolinx handling

--- providers/world4u/stream.ts
+++ providers/world4u/stream.ts
@@ -20,8 +20,6 @@ export const getStream = async function ({
     "Upgrade-Insecure-Requests": "1",
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
-    Cookie:
-      "61cn=1; 61wk=1; __cf_bm=wtv9Eoa2wrUDgevtAnJ6wUOZrxtVYBcddhUDtT0Wj_M-1757137848-1.0.1.1-8Tr7rV19zNgUcRYe_5567LKb2IZrKyxwrc1VWgTmMDd06Givhil3U2kMtUYTYkTnuD3sHUgfh8CO9Y5LrEcZACBbrPE.3Sq5F_JLXaK7Hrw; conv_tracking_data-2=%7B%22mf_source%22%3A%22regular_download-59%22%2C%22mf_content%22%3A%22Free%22%2C%22mf_medium%22%3A%22unknown%5C%2FDefault%20Browser%22%2C%22mf_campaign%22%3A%22616qpccbrq0y4oe%22%2C%22mf_term%22%3A%22d11b8f533377139aa38d757e5057630e%22%7D; ukey=pu2dyp35fyongstav3km969l8d6u2z82",
   };
 
   try {
@@ -64,9 +62,36 @@ export const getStream = async function ({
 
     if (url.includes("photolinx")) {
       console.log("photolinx", url);
+      const photolinxBaseUrl = url.split("/").slice(0, 3).join("/");
+      console.log("photolinxBaseUrl", photolinxBaseUrl);
       // const photolinxBaseUrl = url.split('/').slice(0, 3).join('/');
-      const photolinxRes = await axios.get(url, { headers });
-      const photolinxData = photolinxRes.data;
+      const photolinxRes = await fetch(
+        "https://photolinx.space/download/SzbPKzt6YMO",
+        {
+          headers: {
+            accept:
+              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "accept-language": "en-US,en;q=0.9,en-IN;q=0.8",
+            "cache-control": "no-cache",
+            pragma: "no-cache",
+            priority: "u=0, i",
+            "sec-ch-ua":
+              '"Not;A=Brand";v="99", "Microsoft Edge";v="139", "Chromium";v="139"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Windows"',
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1",
+            cookie:
+              "PHPSESSID=f2211def7938d7228daaa37ffeabcfe0; ext_name=ojplmecpdpgccookcobabopnaifgidhf",
+          },
+          body: null,
+          method: "GET",
+        }
+      );
+      const photolinxData = await photolinxRes.text();
       const $$$ = cheerio.load(photolinxData);
       const access_token = $$$("#generate_url").attr("data-token");
       const uid = $$$("#generate_url").attr("data-uid");
@@ -79,13 +104,25 @@ export const getStream = async function ({
       };
       console.log("photolinxData", JSON.stringify(body));
 
-      const photolinxRes2 = await fetch("https://photolinx.shop/action", {
+      const photolinxRes2 = await fetch(`${photolinxBaseUrl}/action`, {
         headers: {
+          accept: "application/json, text/plain, */*",
+          "accept-language": "en-US,en;q=0.9,en-IN;q=0.8",
+          "cache-control": "no-cache",
+          "content-type": "application/json; charset=UTF-8",
+          pragma: "no-cache",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Not;A=Brand";v="99", "Microsoft Edge";v="139", "Chromium";v="139"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
           "sec-fetch-site": "same-origin",
           "x-requested-with": "xmlhttprequest",
-          cookie: "PHPSESSID=9a8d855c700cf0711831c04960c2e2b4",
-          Referer: "https://photolinx.shop/download/5mPkrBD0D2x",
-          "Referrer-Policy": "strict-origin-when-cross-origin",
+          cookie:
+            "PHPSESSID=f2211def7938d7228daaa37ffeabcfe0; ext_name=ojplmecpdpgccookcobabopnaifgidhf",
+          Referer: url,
         },
         body: JSON.stringify(body),
         method: "POST",
