d001f1e833924c4ee9ed61d42fd125bb4395961a
Dr. Doom <kalipusur1234@gmail.com>
2025-09-11 23:23:44 +0530

    Update catalog.ts

--- providers/cinevood/catalog.ts
+++ providers/cinevood/catalog.ts
@@ -1,1 +1,7 @@
-
+export const catalog = [
+  { title: "Latest", filter: "genre/bollywood" },
+  { title: "Bollywood", filter: "bollywood" },
+  { title: "amazon", filter: "web-series/amazon-web-series" },
+  { title: "hotstar", filter: "web-series/hotstar-web-series" },
+  { title: "tv-shows", filter: "tv-shows" },
+];
