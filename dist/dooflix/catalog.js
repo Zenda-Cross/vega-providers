11597e478e1aa3035dd86580ece0d7fd7aaee4f6
Dr. Doom <kalipusur1234@gmail.com>
2025-09-30 18:19:32 +0530

    Add files via upload

--- dist/dooflix/catalog.js
+++ dist/dooflix/catalog.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.catalog=void 0,exports.catalog=[{title:"Latest",filter:""},{title:"Hollywood",filter:"category/hollywood-movie/"},{title:"Bollywood",filter:"category/uncategorized/"}];
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.catalog=void 0,exports.catalog=[{title:"Trending",filter:""},{title:"Shows",filter:"category/web-series/"}];
