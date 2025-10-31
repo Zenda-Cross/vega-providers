bf2f2d7cbc01e2d8c32773ce45f034c21fb8d2a5
Dr. Doom <kalipusur1234@gmail.com>
2025-10-31 18:32:36 +0530

    Add files via upload

--- providers/GokuHD/catalog.ts
+++ providers/GokuHD/catalog.ts
@@ -1,1 +1,12 @@
+export const catalog = [
+  {
+    title: "Letest",
+    filter: "",
+  },
+  {
+    title: "Hotstar",
+    filter: "category/web-series/disney-plus-hotstar/",
+  },
+];
 
+export const genres = [];
