88e1223d57a15b69907d0d9de8011c1a705051f8
Himanshu <sangwanhimanshu8443@gmail.com>
2026-06-29 11:56:58 +0530

    use common headers

--- providers/uhd/meta.ts
+++ providers/uhd/meta.ts
@@ -1,23 +1,5 @@
 import { Info, Link, ProviderContext } from "../types";
 
-const headers = {
-  Accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-  "Cache-Control": "no-store",
-  "Accept-Language": "en-US,en;q=0.9",
-  DNT: "1",
-  "sec-ch-ua":
-    '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
-  "sec-ch-ua-mobile": "?0",
-  "sec-ch-ua-platform": '"Windows"',
-  "Sec-Fetch-Dest": "document",
-  "Sec-Fetch-Mode": "navigate",
-  "Sec-Fetch-Site": "none",
-  "Sec-Fetch-User": "?1",
-  "Upgrade-Insecure-Requests": "1",
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
-};
 
 async function getWithWAF(
   url: string,
@@ -54,10 +36,10 @@ export const getMeta = async function ({
   providerContext: ProviderContext;
 }): Promise<Info> {
   try {
-    const { axios, cheerio, openWebView } = providerContext;
+    const { axios, cheerio, openWebView, commonHeaders } = providerContext;
     console.log("Fetching metadata from UHD...", link, providerContext);
     const url = link;
-    const res = await getWithWAF(url, axios, openWebView, headers);
+    const res = await getWithWAF(url, axios, openWebView, commonHeaders);
     const html = await res.data;
     const $ = cheerio.load(html);
 
--- providers/uhd/stream.ts
+++ providers/uhd/stream.ts
@@ -1,23 +1,5 @@
 import { ProviderContext, Stream } from "../types";
 
-const headers = {
-  Accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-  "Cache-Control": "no-store",
-  "Accept-Language": "en-US,en;q=0.9",
-  DNT: "1",
-  "sec-ch-ua":
-    '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
-  "sec-ch-ua-mobile": "?0",
-  "sec-ch-ua-platform": '"Windows"',
-  "Sec-Fetch-Dest": "document",
-  "Sec-Fetch-Mode": "navigate",
-  "Sec-Fetch-Site": "none",
-  "Sec-Fetch-User": "?1",
-  "Upgrade-Insecure-Requests": "1",
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
-};
 
 export const getStream = async ({
   link: url,
@@ -27,7 +9,7 @@ export const getStream = async ({
   providerContext: ProviderContext;
 }): Promise<Stream[]> => {
   try {
-    const { axios, cheerio } = providerContext;
+    const { axios, cheerio, commonHeaders: headers } = providerContext;
     let downloadLink = await modExtractor(url, providerContext);
 
     // console.log(downloadLink.data);
